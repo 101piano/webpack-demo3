ebb3e4d25e95f62238c0bbb0ff5fa5bcd654814a
101piano <771594535@qq.com>
2017-04-30 20:30:30 +0800

    框架

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      newTodo:'test',
+      todoList:[
+        {id:1,title:'第一条'},
+        {id:2,title:'第2条'}
+      ]
+    }
   }
+  
+  render(){
+    let todos=this.state.todoList.map((item,index)=>{
+      return <li>{item.title}</li>;     
+    });
+    
+    return (
+      <div className='.App'>
+        <h1>我的待办</h1>
+        <input type='text'/> 
+        <ol>
+          {todos}
+        </ol>
+      </div>
+     
+    )
+    
+  }
+  
+  
+  
 }
 
-export default App;
+export default App
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-
-var arr=[
-   <h1>Hi, world!</h1>,
-   <p>It's a nice day!</p>
-];
 
 ReactDOM.render(
-  <div>{arr}</div>,
+  <App/>,
   document.getElementById('root2')
 );
